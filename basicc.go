209168dc09ebef69c2bb7490d1d7ba96cdbc04f4
Avinash <avinash592004@gmail.com>
2025-06-18 19:04:14 +0530

    step 1

--- basicc.go
+++ basicc.go
@@ -1,1 +1,25 @@
-``
+package main
+
+import f "fmt"
+
+func add(a int, b int) int {
+    f.Println(a + b)
+    return a + b
+}
+
+func main() {
+    f.Println("hello, meow")
+    var name string = "avinash"
+    f.Println(name)
+    age := 18
+    age2 := 20
+    f.Println(age)
+
+    add(age, age2)
+
+	if age >= 18{
+		f.Println("hey bud itme for responsibility")
+	} else {
+		
+	}
+}
