c1ec79802d414f451d609f5b51e72b02f00f7aa3
Avinash <avinash592004@gmail.com>
2025-06-21 20:44:51 +0530

    server

--- cmd/main.go
+++ cmd/main.go
@@ -3,39 +3,39 @@ package main
 import(
 	f "fmt"
 	"net/http"
-	"golangbasics/internals/utils"
+	// "golangbasics/internals/utils"
     ser "golangbasics/internals/server"
 )
 
 func main(){
-	// for array element sum
-	arr := []int {1,2,3,4}
-	f.Println("Array Sum: ", utils.ArrSumElem(arr))
+	// // for array element sum
+	// arr := []int {1,2,3,4}
+	// f.Println("Array Sum: ", utils.ArrSumElem(arr))
 
-	// map implementation
-	var name string
-	f.Print("enter the name: ")
-	f.Scanln(&name)
+	// // map implementation
+	// var name string
+	// f.Print("enter the name: ")
+	// f.Scanln(&name)
 
-	if age, check := utils.MapAge(name); check {
-		f.Printf("%s is maybe %d years old\n", name , age)
-	} else {
-		f.Println("% age not found")
-	}
+	// if age, check := utils.MapAge(name); check {
+	// 	f.Printf("%s is maybe %d years old\n", name , age)
+	// } else {
+	// 	f.Println("% age not found")
+	// }
 
-	//Sort in acending order 
-	nums := []int {7,8,2,4,0,5,0,3,9,3}
-	utils.SortArrayAes(nums)
-	f.Println("Decending order: ")
-	for _, value := range nums {
-		f.Print(value, ",")
-	}
-	//Sort in decending order
-	utils.SortArrayDes(nums)
-	f.Println("Ascending order: ")
-	for _, value := range nums {
-		f.Print(value, ",")
-	}
+	// //Sort in acending order 
+	// nums := []int {7,8,2,4,0,5,0,3,9,3}
+	// utils.SortArrayAes(nums)
+	// f.Println("Decending order: ")
+	// for _, value := range nums {
+	// 	f.Print(value, ",")
+	// }
+	// //Sort in decending order
+	// utils.SortArrayDes(nums)
+	// f.Println("Ascending order: ")
+	// for _, value := range nums {
+	// 	f.Print(value, ",")
+	// }
 	
 	//serverrr
 
